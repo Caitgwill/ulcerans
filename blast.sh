68c2c009e703c7e53dfd8602f7e3c817431816cd
Caitlin Williams <cgwilliams4@gmail.com>
2024-12-09 15:31:03 -0500

    adding in pMUM001 mycolactone toxin plasmid

--- blast.sh
+++ blast.sh
@@ -16,6 +16,8 @@ FNA="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/013/925/GCF_000013925.1_AS
 FAA="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/013/925/GCF_000013925.1_ASM1392v2/GCF_000013925.1_ASM1392v2_protein.faa.gz"
 H37RVfna="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/195/955/GCF_000195955.2_ASM19595v2/GCF_000195955.2_ASM19595v2_genomic.fna.gz"
 H37Rvfaa="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/195/955/GCF_000195955.2_ASM19595v2/GCF_000195955.2_ASM19595v2_protein.faa.gz"
+pMUM001fna="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/013/925/GCF_000013925.1_ASM1392v2/GCF_000013925.1_ASM1392v2_genomic.fna.gz"
+pMUM001faa="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/013/925/GCF_000013925.1_ASM1392v2/GCF_000013925.1_ASM1392v2_protein.faa.gz"
 
 if [ ! -d $OUTDIR ]
 then
@@ -35,6 +37,10 @@ cd $OUTDIR
 #curl -s $H37RVfna | gunzip -c > $OUTDIR/H37RV.fna
 #curl -s $H37Rvfaa | gunzip -c > $OUTDIR/H37Rv.faa
 
+#download pMUM001 ref seq
+curl -s $pMUM001fna | gunzip -c > $OUTDIR/pMUM001.fna
+curl -s $pMUM001faa | gunzip -c > $OUTDIR/pMUM001.faa
+
 #check that downloads were successful before moving to blast.
 #if [[ ! -f $OUTDIR/mulcerans.fna || ! -f $OUTDIR/mulcerans.faa || ! -f $OUTDIR/H37RV.fna || ! -f $OUTDIR/H37Rv.faa ]]; then
 #    echo "Error: One or more files failed to download." >&2
@@ -48,10 +54,14 @@ cd $OUTDIR
 
 #blast ref nucleotide sequence to mtb database. Look for high scoring pairs (HSPs) and percent identity to evaluate homology.
 #Use BLAST viewer or Biopython for result visualization if needed. Outfmt 6 = tabular out put. outfmt 5 is an XML for more detailed records.
-blastn -query $OUTDIR/mulcerans.fna -db $OUTDIR/mtb_fna_db -out $OUTDIR/blast_fna_results.out -outfmt 7 -num_threads 8 &
+#blastn -query $OUTDIR/mulcerans.fna -db $OUTDIR/mtb_fna_db -out $OUTDIR/blast_fna_results.out -outfmt 7 -num_threads 8 &
 
 #blast ref protein sequence to mtb database. 
-blastp -query $OUTDIR/H37Rv.faa -db $OUTDIR/mtb_protein_db -out $OUTDIR/blast_protein_results.out -outfmt 7 -evalue 1e-5 -num_threads 8 &
+#blastp -query $OUTDIR/H37Rv.faa -db $OUTDIR/mtb_protein_db -out $OUTDIR/blast_protein_results.out -outfmt 7 -evalue 1e-5 -num_threads 8 &
+
+#blast pMUM001 against mtb db.
+blastn -query $OUTDIR/pMUM001.fna -db $OUTDIR/mtb_fna_db -out $OUTDIR/blast_pMUM001fna_results.out -outfmt 7 -num_threads 8 &
+blastp -query $OUTDIR/pMUM001.faa -db $OUTDIR/mtb_protein_db -out $OUTDIR/blast_pMUM001protein_results.out -outfmt 7 -evalue 1e-5 -num_threads 8 &
 
 #wait until both processes are finished.
-wait
+#wait
